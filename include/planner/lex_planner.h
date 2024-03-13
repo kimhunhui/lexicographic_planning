ff844fe3a3c994f02ba67abbc675d796b634fe9b
kimhunhui <51473837+kimhunhui@users.noreply.github.com>
2024-03-13 10:31:13 +0900

    Update lex_planner.h

--- include/planner/lex_planner.h
+++ include/planner/lex_planner.h
@@ -1,69 +1,46 @@
+#ifndef LEX_PLANNER_HPP
+#define LEX_PLANNER_HPP
+
 #include "rclcpp/rclcpp.hpp"
 #include "nav2_core/global_planner.hpp"
-#include "tf2_ros/transform_listener.h"
-#include "tf2_ros/buffer.h"
-#include "nav_msgs/msg/path.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
 #include <mutex>
-
-using std::placeholders::_1;
 
 namespace lex_planner {
 
 class LEXPlanner : public nav2_core::GlobalPlanner {
 public:
-    rclcpp::Node::SharedPtr node_;
-
-    std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
-    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-
-    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr subPath;
-    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr pubGoal;
-
-    // For visualizing twist commands
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subTwistCommand1;
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subTwistCommand2;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pubTwistCommand;
-
-    nav_msgs::msg::Path globalPath;
-
-    std::mutex mtx;
-
-    LEXPlanner()
-    : LEXPlanner("", nullptr) {}
-
-    LEXPlanner(const std::string& name, const std::shared_ptr<tf2_ros::Buffer>& tf)
-    : tf_buffer_(tf) {
-        (void)name; // Unused parameter for now
-    }
-
+    LEXPlanner();
+    explicit LEXPlanner(const rclcpp::Node::SharedPtr& node);
     void configure(const rclcpp_lifecycle::LifecycleNode::SharedPtr& parent,
                    std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
-                   std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override {
-        node_ = parent;
-        tf_buffer_ = tf;
-        // Initialize subscriptions, publishers, etc.
-    }
+                   std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
 
-    void pathHandler(const nav_msgs::msg::Path::SharedPtr pathMsg) {
-        // Handle path
-    }
+    void cleanup() override;
+    void activate() override;
+    void deactivate() override;
 
-    // Visualize twist command
-    void twistCommandHandler(const geometry_msgs::msg::Twist::SharedPtr msg) {
-        // Handle twist command
-    }
+    void pathCallback(const nav_msgs::msg::Path::SharedPtr pathMsg);
+    void twistCommandCallback(const geometry_msgs::msg::Twist::SharedPtr twistMsg);
 
-    bool makePlan(const geometry_msgs::msg::PoseStamped& start,
-                  const geometry_msgs::msg::PoseStamped& goal,
-                  std::vector<geometry_msgs::msg::PoseStamped>& plan) override {
-        // Implement planning logic
-        return true;
-    }
+    bool createPlan(const geometry_msgs::msg::PoseStamped& start,
+                    const geometry_msgs::msg::PoseStamped& goal,
+                    std::vector<geometry_msgs::msg::PoseStamped>& plan) override;
+
+private:
+    rclcpp::Node::SharedPtr node_;
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr subPath_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pubGoal_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pubTwistCommand_;
+
+    std::mutex mutex_;
+    nav_msgs::msg::Path globalPath_;
+    std::shared_ptr<tf2_ros::Buffer> tf_;
 };
 
-}; // namespace lex_planner
+}
 
-#include "rclcpp_components/register_node_macro.hpp"
-
-RCLCPP_COMPONENTS_REGISTER_NODE(lex_planner::LEXPlanner)
+#endif // LEX_PLANNER_HPP
